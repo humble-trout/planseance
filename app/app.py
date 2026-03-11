636ac338e61d42e8f4e94d1159d41d42c344df68
humble-trout <beatricepascale249@gmail.com>
2026-03-11 19:24:46 +0100

    [edit] paramétrage de l'appli, en cours

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+#importations dans l'appli de rencontre Flask qui nous sert de modèle + celles de Maxime Challon
+#à voir si on supprime certaines des importations Flask jsp à quoi elles servent toutes
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 
 app = Flask(
     __name__, 
